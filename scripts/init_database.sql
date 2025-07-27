5a8d97d28cf72f6df3afdd2a3233cb940803ee18
radwanagy <112013075+radwanagy@users.noreply.github.com>
2025-07-27 14:54:21 +0300

    create database and schema

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,43 @@
 
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'BankingDB' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'BankingDB' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'BankingDB' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'BankingDB')
+BEGIN
+    ALTER DATABASE BankingDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE BankingDB;
+END;
+GO
+
+-- Create the 'BankingDB' database
+CREATE DATABASE BankingDB;
+GO
+
+USE BankingDB;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
